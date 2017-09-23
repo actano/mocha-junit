a6ef47016dad7f4117551c3cc40a566a30c1486d
Marcus Mennemeier <marcus.mennemeier@actano.de>
2017-09-23 15:04:27 +0200

    Write test stubs for contract of readme.md

---
+++ test/spec.js
@@ -0,0 +1,9 @@
+import { describe, it } from 'mocha'
+
+describe('contract from readme', () => {
+  it('should write result file and exit with zero, when tests are successful')
+  it('should write result file and exit with zero, when tests aren\'t succesful')
+  it('should capture stdout from tests')
+  it('should capture stderr from tests')
+  it('should exit with non-zero code with an exception outside of tests')
+})
