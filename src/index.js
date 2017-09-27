ad9c9761e3e3cc69c1c590d72cb99a15487b7e8c
Marcus Mennemeier <marcus.mennemeier@actano.de>
2017-09-27 18:12:19 +0200

    Move stream handling to separate file

--- src/index.js
+++ src/index.js
@@ -6,54 +6,9 @@ import Mocha from 'mocha'
 import path from 'path'
 import Testsuite from './testsuite'
 import Test from './test'
+import { captureStreams, releaseStreams, withReleaseStreams } from './streams'
 
 const { REPORT_FILE } = process.env
-
-function captureStream(test, name, stream) {
-  const oldWrite = stream.write
-  const buf = test[name]
-  stream.write = function streamWrite(chunk, encoding, cb) {
-    // param encoding could be ommitted
-    if (typeof encoding === 'function') {
-      cb = encoding
-      encoding = null
-    }
-
-    buf.push(chunk.toString())
-
-    // call the original function to get output also on stdout/stderr of the process as usual
-    oldWrite.call(stream, chunk, encoding, (...args) => {
-      // call callback if callback is given
-      if (typeof cb === 'function') {
-        cb(...args)
-      }
-    })
-  }
-
-  stream.write._old = oldWrite
-}
-
-function releaseStream(stream) {
-  if (stream.write._old == null) {
-    return
-  }
-  stream.write = stream.write._old
-}
-
-function captureStreams(test) {
-  captureStream(test, 'system-out', process.stdout)
-  captureStream(test, 'system-err', process.stderr)
-}
-
-function releaseStreams() {
-  releaseStream(process.stdout)
-  releaseStream(process.stderr)
-}
-
-const withReleaseStreams = fn => (...args) => {
-  releaseStreams()
-  fn(...args)
-}
 
 const install = (reportFile) => {
   const name = path.basename(reportFile, path.extname(reportFile)).replace(/\//g, '.')
