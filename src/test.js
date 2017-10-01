6ccdb8fb565564c98a1daa5da1b19a8e067d0d5c
Marcus Mennemeier <marcus.mennemeier@actano.de>
2017-10-01 15:24:41 +0200

    Make writing of tests stateless

--- src/test.js
+++ src/test.js
@@ -2,6 +2,29 @@ import { writeSystemErr, writeSystemOut } from './has-streams'
 import fullTitle from './mocha/full-title'
 import { closeTag, emptyTag, openTag, text } from './xml-writer'
 
+export function writeTest(writable, test, failures, suiteName, stdout, stderr) {
+  const skipped = !failures.length && test.isPending()
+  const attrs = {
+    classname: `${suiteName}.${fullTitle(test.parent)}`,
+    name: test.title,
+  }
+  if (!skipped) {
+    attrs.time = test.duration / 1000
+  }
+  openTag(writable, 'testcase', attrs)
+  for (const failure of failures) {
+    openTag(writable, 'failure', { message: failure.message })
+    text(writable, failure.content)
+    closeTag(writable, 'failure')
+  }
+  if (skipped) {
+    emptyTag(writable, 'skipped')
+  }
+  writeSystemOut(writable, stdout)
+  writeSystemErr(writable, stderr)
+  closeTag(writable, 'testcase')
+}
+
 export default class Test {
   constructor(test) {
     this['system-out'] = []
@@ -23,25 +46,6 @@ export default class Test {
   }
 
   write(writable, suiteName) {
-    const skipped = this.isSkipped()
-    const attrs = {
-      classname: `${suiteName}.${fullTitle(this.test.parent)}`,
-      name: this.test.title,
-    }
-    if (!skipped) {
-      attrs.time = this.test.duration / 1000
-    }
-    openTag(writable, 'testcase', attrs)
-    for (const failure of this.failures) {
-      openTag(writable, 'failure', { message: failure.message })
-      text(writable, failure.content)
-      closeTag(writable, 'failure')
-    }
-    if (skipped) {
-      emptyTag(writable, 'skipped')
-    }
-    writeSystemOut(writable, this['system-out'].join('').trim())
-    writeSystemErr(writable, this['system-err'].join('').trim())
-    closeTag(writable, 'testcase')
+    writeTest(writable, this.test, this.failures, suiteName, this['system-out'].join('').trim(), this['system-err'].join('').trim())
   }
 }
