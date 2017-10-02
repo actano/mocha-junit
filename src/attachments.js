4f81745a27accdf1a155446300c03db14f670693
Marcus Mennemeier <marcus.mennemeier@actano.de>
2017-10-02 20:40:45 +0200

    Improve usage of resolve, to allow absolute filenames

--- src/attachments.js
+++ src/attachments.js
@@ -1,14 +1,12 @@
 /* eslint-disable no-param-reassign */
 
-import { resolve, dirname, join } from 'path'
+import { resolve, dirname } from 'path'
 import output from './lib/output'
-
-export const outputDir = resolve(dirname(output))
 
 // https://wiki.jenkins.io/display/JENKINS/JUnit+Attachments+Plugin#JUnitAttachmentsPlugin-Byputtingthemintoaknownlocation
 
 const addAttachment = (test, filename) => {
-  const target = join(outputDir, filename)
+  const target = resolve(dirname(output), filename)
   if (!test.junitAttachments) {
     test.junitAttachments = []
   }
